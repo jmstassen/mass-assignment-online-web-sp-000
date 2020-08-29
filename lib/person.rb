7daf7f9ffdddb8d9f9032343370a56598ca81fe8
John Stassen <john.stassen@gmail.com>
2020-08-29 17:48:08 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,6 +1,6 @@
 class Person
-  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, 
-:weight, :handed, :complexion, :t_shirt_size, 
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
+:weight, :handed, :complexion, :t_shirt_size,
 :wrist_size, :glove_size, :pant_length, :pant_width
 
   def initialize(attributes)
@@ -8,5 +8,5 @@ class Person
     
   end
 
-  
+
 end
